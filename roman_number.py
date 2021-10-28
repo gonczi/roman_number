079f18709c4aaf8b6ba8ca009461c7903f26a6f5
Zoltan Gonczi <zoltan.gonczi@gmail.com>
2021-10-28 11:02:51 +0200

    input param contains valid chars

--- roman_number.py
+++ roman_number.py
@@ -6,4 +6,8 @@ def parse(s):
     if len(s.strip()) == 0:
         return -1
 
+    for ch in list(s):
+        if ch not in ['I', 'V', 'X', 'L', 'C', 'D', 'M']:
+            return -1
+
     return 0
--- test_roman_number.py
+++ test_roman_number.py
@@ -4,14 +4,18 @@ import roman_number
 
 class TestParse(TestCase):
     def test_parse_empty_string(self):
-        num = roman_number.parse("")
+        num = roman_number.parse(" ")
         self.assertEqual(num, -1)
 
     def test_parse_not_a_string(self):
         num = roman_number.parse(None)
         self.assertEqual(num, -1)
 
-    def test_parse_not_a_roman_number(self):
-        num = roman_number.parse("None")
+    def test_parse_contains_roman_numbers_only(self):
+        num = roman_number.parse("IICCCMMMX")
+        self.assertNotEqual(num, -1)
+
+    def test_parse_contains_roman_numbers_only_neg(self):
+        num = roman_number.parse("IICCCMMMeX")
         self.assertEqual(num, -1)
 
